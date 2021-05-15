4f5b4550751178061ea1023c7dfbeaff52c8cc9a
ravi2015t <tejcoo@gmail.com>
2021-05-15 16:13:00 -0400

    Implemented Log; Writes/Reads from active segments and makes an entry on the corresponding index

--- internal/log/segment_test.go
+++ internal/log/segment_test.go
@@ -1,10 +1,12 @@
 package log
 
 import (
+	"io"
 	"io/ioutil"
 	"os"
 	"testing"
 
+	api "github.com/ravi2015t/proglog/api/v1"
 	"github.com/stretchr/testify/require"
 )
 
@@ -12,9 +14,42 @@ func TestSegment(t *testing.T) {
 	dir, _ := ioutil.TempDir("", "segment-test")
 	defer os.RemoveAll(dir)
 
+	want := &api.Record{Value: []byte("test segment")}
+
 	c := Config{}
 	c.Segment.MaxStoreBytes = 1024
 	c.Segment.MaxIndexBytes = entWidth * 3
-	_, err := newSegment(dir, 16, c)
+	s, err := newSegment(dir, 16, c)
 	require.NoError(t, err)
+	require.Equal(t, uint64(16), s.nextOffset, s.nextOffset)
+
+	for i := uint64(0); i < 3; i++ {
+		t.Logf("base offset :%d next offset :%d", s.baseOffset, s.nextOffset)
+		off, err := s.Append(want)
+		require.NoError(t, err)
+		require.Equal(t, 16+i, off)
+		got, err := s.Read(off)
+		require.NoError(t, err)
+		t.Logf("want %s got %s", want.Value, got.Value)
+		require.Equal(t, want.Value, got.Value)
+	}
+
+	_, err = s.Append(want)
+	require.Error(t, io.EOF, err)
+
+	require.True(t, s.IsMaxed())
+
+	c.Segment.MaxStoreBytes = uint64(len(want.Value) * 3)
+	c.Segment.MaxIndexBytes = 1024
+
+	s, err = newSegment(dir, 16, c)
+	require.NoError(t, err)
+
+	require.True(t, s.IsMaxed())
+
+	err = s.Remove()
+	require.NoError(t, err)
+	s, err = newSegment(dir, 16, c)
+	require.NoError(t, err)
+	require.False(t, s.IsMaxed())
 }
